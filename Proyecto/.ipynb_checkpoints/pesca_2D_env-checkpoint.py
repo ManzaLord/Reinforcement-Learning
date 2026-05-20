152f56da56f30d0215d7df20c902c7bb50ed875a
ManzaLord <fxymanzar3@gmail.com>
2026-05-19 21:38:08 -0600

    Harvest y bugfix

--- Proyecto/.ipynb_checkpoints/pesca_2D_env-checkpoint.py
+++ Proyecto/.ipynb_checkpoints/pesca_2D_env-checkpoint.py
@@ -17,9 +17,10 @@ class Pesca2D(gym.Env):
         self.eps = params["epsilon"]
 
         self.observation_space = gym.spaces.Box(
-            low=np.array([-1.0], dtype=np.float32),
-            high=np.array([1.0], dtype=np.float32),
-            dtype=np.float32
+        low=-1.0,
+        high=1.0,
+        shape=(2,),
+        dtype=np.float32
         )
 
         self.action_space = gym.spaces.Box(
@@ -36,26 +37,30 @@ class Pesca2D(gym.Env):
 
         self.t = 0
 
-        return np.array([self.normalize(self.B)], dtype=np.float32), {}
+        observation = np.array([self.normalize(self.B[0]), self.normalize(self.B[1])], dtype=np.float32)
+
+        return observation, {}
 
     def step(self, action):
         action = float(action[0])
 
         harvest_fraction = self.unnormalize(action)
-        harvest = self.B * harvest_fraction
+        harvest = self.B[0] * harvest_fraction
 
         # dinámica
-        self.B[0] = self.B[0] + self.r[0] * self.B[0] (1 - self.B[0]) - self.B[0] * self.B[1] * self.mort[0]
+        B_temp = np.copy(self.B[0])
+        
+        self.B[0] = self.B[0] + (self.r[0] * self.B[0] * (1 - self.B[0]) - self.B[0] * self.B[1] * self.mort[0]) * np.random.normal(1,0.4) - harvest
 
-        self.B[1] = self.B[1] + self.r[1] * self.B[1] * (1 - self.B[1] / (self.B[0] * self.C[1] + self.eps))
+        self.B[1] = self.B[1] + self.r[1] * self.B[1] * (1 - self.B[1] / (B_temp * self.C + self.eps)) * np.random.normal(1,0.4)
         
         #self.B += self.sigma * self.np_random.normal()
 
         self.B = np.clip(self.B, 0.0, 1.0)
 
-        reward = 0.1 * self.B[1]
+        reward = 0.1 * self.B[1] +  harvest
 
-        observation = np.array([self.normalize(self.B[0])], dtype=np.float32)
+        observation = np.array([self.normalize(self.B[0]), self.normalize(self.B[1])], dtype=np.float32)
 
         self.t += 1
 
