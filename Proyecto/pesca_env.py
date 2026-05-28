3ed525481ae87968e1ef8e9dfdd9be84915655bc
Esteban Porras Marín <porras.marin.esteban@gmail.com>
2026-05-28 14:16:15 -0600

    Implementación de gráficas para 4D (deben arreglarse)

--- Proyecto/pesca_env.py
+++ Proyecto/pesca_env.py
@@ -1,6 +1,7 @@
 import gymnasium as gym
 import numpy as np
 
+#Una sola población
 class Pesca1D(gym.Env):
     def __init__(self, r, sigma=0.05, init_B=0.5, T=200):
         self.r = r
@@ -64,7 +65,7 @@ class Pesca1D(gym.Env):
     def unnormalize(self, qty):
         return (qty + 1) / 2
 
-
+#Un depredador y una presa
 class Pesca2D(gym.Env):
     def __init__(self, params):
         self.r = params["r_crecimiento"]
@@ -90,6 +91,10 @@ class Pesca2D(gym.Env):
 
     def reset(self, *, seed=None, options=None):
         super().reset(seed=seed)
+
+        print(self.init_B)
+        print(self.sigma)
+        print(self.np_random.normal())
 
         self.B = self.init_B + self.sigma * self.np_random.normal()
         self.B = np.clip(self.B, 0.0, 1.0)
@@ -134,11 +139,7 @@ class Pesca2D(gym.Env):
     def unnormalize(self, qty):
         return (qty + 1) / 2
 
-
-    import gymnasium as gym
-import numpy as np
-
-#Dos depredadores y dos presas
+#Dos depredadores y dos presas [Presa1, Presa2, Depredador1, Depredador2]
 class Pesca4D(gym.Env):
     def __init__(self, params):
         self.r = params["r_crecimiento"]
@@ -148,6 +149,7 @@ class Pesca4D(gym.Env):
         self.mort = params["mortalidad"]
         self.C = params["C"]
         self.eps = params["epsilon"]
+        self.arrastre = params["arrastre"]
 
         self.observation_space = gym.spaces.Box(
         low=-1.0,
@@ -165,12 +167,12 @@ class Pesca4D(gym.Env):
     def reset(self, *, seed=None, options=None):
         super().reset(seed=seed)
 
-        self.B = self.init_B + self.sigma * self.np_random.normal()
+        self.B = self.init_B + self.sigma * self.np_random.normal(1,0.4)
         self.B = np.clip(self.B, 0.0, 1.0)
 
         self.t = 0
 
-        observation = np.array([self.normalize(self.B[0])], dtype=np.float32)
+        observation = np.array([self.normalize(self.B[0]), self.normalize(self.B[1]), self.normalize(self.B[2]), self.normalize(self.B[3])], dtype=np.float32)
 
         return observation, {}
 
@@ -184,16 +186,16 @@ class Pesca4D(gym.Env):
         B_init = np.copy(self.B)
 
         #Evolucion de B0
-        self.B[0] = self.r[0] * B_init[0] * (1 - B_init[0] / (1 - """"a""" * B_init[1])) - sel.mort[0] * B_init[0] * B_init[3]
+        self.B[0] = B_init[0] + self.r[0] * B_init[0] * (1 - B_init[0] / (1 - self.arrastre[0] * B_init[1])) - self.mort[0] * B_init[0] * B_init[3] - harvest
 
         #Evolucion de B1
-        self.B[1] = self.r[1] * B_init[1] * (1 - B_init[1] / (1 - """"a""" * B_init[0])) - self.mort[1] * B_init[1] * B_init[2]
+        self.B[1] = B_init[1] + self.r[1] * B_init[1] * (1 - B_init[1] / (1 - self.arrastre[0] * B_init[0])) - self.mort[1] * B_init[1] * B_init[2]
 
         #Evolucion de B2
-        self.B[2] = self.r[2] * B_init[2] * B_init[1] (1 - B_init[2] / (1 - """"b""" * B_init[3]))
+        self.B[2] = B_init[2] + self.r[2] * B_init[2] * B_init[1] * (1 - B_init[2] / (1 - self.arrastre[1] * B_init[3]))
 
         #Evolucion de B3
-        self.B[3] = self.r[3] * B_init[3] * B_init[0] (1 - B_init[3] / (1 - """"b""" * B_init[2]))
+        self.B[3] = B_init[3] + self.r[3] * B_init[3] * B_init[0] * (1 - B_init[3] / (1 - self.arrastre[1] * B_init[2]))
 
         #Normaliza las evoluciones
         self.B = np.clip(self.B, 0.0, 1.0)
@@ -202,7 +204,7 @@ class Pesca4D(gym.Env):
         reward = harvest + 0.2 * self.B[3]
 
         #Define la observacion que devuelve
-        observation = np.array([self.normalize(self.B[0])], dtype=np.float32)
+        observation = np.array([self.normalize(self.B[0]), self.normalize(self.B[1]), self.normalize(self.B[2]), self.normalize(self.B[3])], dtype=np.float32)
 
         #Evolucion temporal
         self.t += 1
