06e9ae8c800b26165a1de06e2ae354ba3b3bf92b
Esteban Porras Marín <porras.marin.esteban@gmail.com>
2026-07-06 19:51:56 -0600

    Cluster2

--- Proyecto/PPO_4/run_PPO4.sh
+++ Proyecto/PPO_4/run_PPO4.sh
@@ -2,11 +2,11 @@
 #SBATCH --partition=serial
 #SBATCH --time=3:00:00
 #SBATCH --ntasks=1
-#SBATCH --job-name="pesca"
+#SBATCH --job-name="pesca3"
 #SBATCH --output=pesca.out.%j
 #SBATCH --mail-user=esteban.porrasmarin@ucr.ac.cr
 #SBATCH --mail-type=END,FAIL
 
 mamba activate pesca_env
 
-python3 Cluster_PPO.py
+python3 Cluster_PPO4.py
